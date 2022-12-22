d5104e4b8e07db818e1e2c60470f07a62cc86a0c
sawsew467 <bao.thang.1912@gmail.com>
2022-12-22 15:18:08 +0700

    first commit

---
+++ components/LoginModal/LoginModal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+function LoginModal() {
+  return (
+    <>
+      <div className={styles.overlay}>fwef</div>
+    </>
+  );
+}
+
+export default LoginModal;
