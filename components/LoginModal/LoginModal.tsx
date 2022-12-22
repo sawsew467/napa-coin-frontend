4cc665c70ab56738f61f25738fb223475120e76d
sawsew467 <bao.thang.1912@gmail.com>
2022-12-22 16:36:20 +0700

    test

--- components/LoginModal/LoginModal.tsx
+++ components/LoginModal/LoginModal.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function LoginModal() {
-  return <div className="overlay">LoginModal</div>;
+  return (
+    <div className="overlay">
+      <div className="box-modal">dawd</div>
+    </div>
+  );
 }
 
 export default LoginModal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({ weight: "100" })
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <main className={poppins.className}>
+  <Component {...pageProps} />
+  </main>
+  </>
 }
