bbfcd111138631431391079da104c2b590abc591
le-toan-1 <85981385+le-toan-1@users.noreply.github.com>
2022-12-29 13:58:15 +0700

    update pagination

--- components/Trending/index.tsx
+++ components/Trending/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 
 import style from './style.module.scss';
+import coiImg from '../../assets/img/avt.png';
+
 const Trending = () => {
-    return <div className={style[`trending`]}> </div>;
+    return (
+        <div className={style[`trending`]}>
+            <div className={style[`trending__coin`]}>
+                <Image src={coiImg} alt="" className={style[`symbol-image`]} />
+                <h1 className={style[`symbol-name`]}>BitTorrent</h1>
+            </div>
+            <div className={style[`trending__rank`]}>
+                <p className={style[`trending__rank--pos`]}># 1</p>
+            </div>
+        </div>
+    );
 };
 
 export default Trending;
--- pages/token-detail/index.tsx
+++ pages/token-detail/index.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
+
 import Header from '../../components/Header/Header';
 import DetailToken from '../../components/DetailToken';
 import Convert from '../../components/Convert';
+import Trending from '../../components/Trending';
+import style from './style.module.scss';
+import liveChart from '../../assets/img/live-chart.png';
+import Image from 'next/image';
 
 const TokenDetail = () => {
     return (
         <>
-            <Header></Header>
+            <Header
+                setIsShowLoginModal={function (value: React.SetStateAction<boolean>): void {
+                    throw new Error('Function not implemented.');
+                }}
+            ></Header>
             <div className="bg_home">
                 <DetailToken></DetailToken>
                 <h1 className="title-home">BTT to USD Converter </h1>
-                <Convert></Convert>
+                <div className={style[`convert-section`]}>
+                    <div className={style[`chart-col`]}>
+                        <div className={style[`convert-coin`]}>
+                            <Convert></Convert>
+                        </div>
+                        <div className={style[`live__chart`]}>
+                            <Image src={liveChart} alt="" />
+                        </div>
+                    </div>
+                    <div className={style[`trending-token`]}>
+                        <h2>Trending Coins and Tokens 🔥</h2>
+                        <div className={style[`trending-item`]}>
+                            <Trending></Trending>
+                            <Trending></Trending>
+                            <Trending></Trending>
+                            <Trending></Trending>
+                            <Trending></Trending>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
