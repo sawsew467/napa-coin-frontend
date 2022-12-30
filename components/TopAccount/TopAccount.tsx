0fe6209d14e352fe737487b9f84d9053b02f6b57
le-toan-1 <85981385+le-toan-1@users.noreply.github.com>
2022-12-30 13:29:51 +0700

    dump data into table

--- components/TopAccount/TopAccount.tsx
+++ components/TopAccount/TopAccount.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import style from './top-account.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 import Account from './Account';
+import style from './top-account.module.scss';
 
 const TopAccount = () => {
     return (
@@ -13,9 +18,36 @@ const TopAccount = () => {
                     <FontAwesomeIcon className={style.star} icon={faStar} /> Top Community Accounts
                 </h2>
                 <div className={style.account__flex}>
-                    <Account></Account>
-                    <Account></Account>
-                    <Account></Account>
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={96}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        centeredSlides={true}
+                        modules={[Autoplay, Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <Account></Account>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Account></Account>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Account></Account>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Account></Account>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Account></Account>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </div>
